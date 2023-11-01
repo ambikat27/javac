66e4d9e188f540992f91ad58b108492de4f7ceb3
Ambikaat <ambikat.xworkz@gmail>
2023-11-01 09:33:22 +0530

    baginfo

---
+++ Bag.java
@@ -0,0 +1,337 @@
+class Bags
+{
+public static void main(String[] args)
+{
+	
+int[] no={1,2,3,4,5,6,7,8,9,10};
+System.out.println(no[3]);
+
+System.out.println("==============");
+no[2]=1;
+System.out.println(no[2]);
+no[4]=3;
+System.out.println(no[4]);
+no[5]=4;
+System.out.println(no[5]);
+no[6]=5;
+System.out.println(no[6]);
+System.out.println("==============");
+
+for(int i=0;i<no.length;i++)
+{
+	System.out.println(no[i]);
+}
+System.out.println("=========");
+for(int i=no.length-1;i>0;i--)
+{
+	System.out.println(no[i]);
+
+}
+System.out.println("=========");
+
+for(int i=0;i<no.length;i=i+2)
+{
+	System.out.println(no[i]);
+}
+System.out.println("=========");
+for(int j=0;j<no.length/2;j++)
+{
+	System.out.println(no[j]);
+}
+System.out.println("=========");
+for(int i=3;i<7;i++)
+{
+  System.out.println(no[i]);
+}
+
+
+System.out.println("**************");
+String name[]={"latha","Sneha","Manasa","sush","vinu","teju","tomy","jerry","bunny"};
+name[2]="puppy";
+System.out.println(name[2]);
+name[4]="kruthi";
+System.out.println(name[4]);
+name[5]="nithy";
+System.out.println(name[5]);
+name[6]="sonu";
+System.out.println(name[6]);
+System.out.println("=========");
+for(int i=0;i<name.length;i++)
+{
+System.out.println(name[i]);
+}
+System.out.println("=========");
+for(int i=name.length-1;i>0;i--)
+{
+System.out.println(name[i]);
+}
+System.out.println("=========");
+for(int i=0;i<name.length/2;i++)
+{
+System.out.println(name[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<name.length;i=i+2)
+{
+ System.out.println(name[i]);
+}
+
+
+System.out.println("*******************");
+char[] letter={'a','b','c','d','e','f','g','h','i','j'};
+letter[2]='e';
+System.out.println(letter[2]);
+letter[4]='k';
+System.out.println(letter[4]);
+letter[5]='s';
+System.out.println(letter[5]);
+letter[6]='h';
+System.out.println(letter[6]);
+System.out.println("=========");
+for(int i=0;i<letter.length;i++)
+{
+System.out.println(letter[i]);
+}
+System.out.println("=========");
+for(int i=letter.length-1;i>0;i--)
+{
+System.out.println(letter[i]);
+}
+System.out.println("=========");
+for(int i=0;i<letter.length/2;i++)
+{
+System.out.println(letter[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<letter.length;i=i+3)
+{
+ System.out.println(letter[i]);
+}
+
+
+System.out.println("**************");
+double[] marks={22,33,44,55,66,77,88.66,99,78,90};
+marks[1]=11;
+System.out.println(marks[1]);
+marks[3]=33;
+System.out.println(marks[3]);
+marks[5]=44;
+System.out.println(marks[5]);
+marks[7]=55;
+System.out.println(marks[7]);
+System.out.println("==============");
+for(int i=0;i<marks.length;i++)
+{
+System.out.println(marks[i]);
+}
+System.out.println("=========");
+for(int i=marks.length-1;i>0;i--)
+{
+System.out.println(marks[i]);
+}
+System.out.println("=========");
+for(int i=0;i<marks.length/2;i++)
+{
+System.out.println(marks[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<marks.length;i=i+3)
+{
+ System.out.println(marks[i]);
+}
+
+
+System.out.println("****************");
+float cost[]={11,21,31,41,51,61,71,81,91,100};
+cost[2]=1;
+System.out.println(cost[2]);
+cost[4]=3;
+System.out.println(cost[4]);
+cost[5]=4;
+System.out.println(cost[5]);
+cost[6]=5;
+System.out.println(cost[6]);
+System.out.println("==============");
+
+for(int i=0;i<cost.length;i++)
+{
+System.out.println(cost[i]);
+}
+System.out.println("=========");
+for(int i=cost.length-1;i>0;i--)
+{
+System.out.println(cost[i]);
+}
+System.out.println("=========");
+for(int i=0;i<cost.length/2;i++)
+{
+System.out.println(cost[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<cost.length;i=i+3)
+{
+ System.out.println(cost[i]);
+}
+
+
+
+System.out.println("****************");
+byte[] childerns={1,3,4,5,6,7,4,8,9,32};
+childerns[2]=1;
+System.out.println(childerns[2]);
+childerns[4]=3;
+System.out.println(childerns[4]);
+childerns[5]=4;
+System.out.println(childerns[5]);
+childerns[6]=5;
+System.out.println(childerns[6]);
+for(int i=0;i<childerns.length;i++)
+{
+System.out.println(childerns[i]);
+}
+System.out.println("=========");
+for(int i=childerns.length-1;i>0;i--)
+{
+System.out.println(childerns[i]);
+}
+System.out.println("=========");
+for(int i=0;i<childerns.length/2;i++)
+{
+System.out.println(childerns[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<childerns.length;i=i+3)
+{
+ System.out.println(childerns[i]);
+}
+
+
+System.out.println("****************");
+long[] prime={2,4,6,8,10,12,14,16,18,20};
+prime[2]=1;
+System.out.println(prime[2]);
+prime[4]=3;
+System.out.println(prime[4]);
+prime[5]=4;
+System.out.println(prime[5]);
+prime[6]=5;
+System.out.println(prime[6]);
+System.out.println("=========");
+for(int i=0;i<prime.length;i++)
+{
+System.out.println(prime[i]);
+}
+System.out.println("=========");
+for(int i=prime.length-1;i>0;i--)
+{
+System.out.println(prime[i]);
+}
+System.out.println("=========");
+for(int i=0;i<prime.length/2;i++)
+{
+System.out.println(prime[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<prime.length;i=i+3)
+{
+ System.out.println(prime[i]);
+}
+
+
+System.out.println("****************");
+short[] age ={23,34,56,78,98,89,56,44,45,76};
+age[2]=1;
+System.out.println(age[2]);
+age[4]=3;
+System.out.println(age[4]);
+age[5]=4;
+System.out.println(age[5]);
+age[6]=5;
+System.out.println(age[6]);
+System.out.println("=========");
+for(int i=0;i<age.length;i++)
+{
+System.out.println(age[i]);
+}
+System.out.println("=========");
+for(int i=age.length-1;i>0;i--)
+{
+System.out.println(age[i]);
+}
+System.out.println("=========");
+for(int i=0;i<age.length/2;i++)
+{
+System.out.println(age[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<age.length;i=i+3)
+{
+ System.out.println(age[i]);
+}   
+
+  
+System.out.println("*******************");
+char[] alphabets={'e','r','t','o','p','k','x','n','m','g'};
+alphabets[2]='e';
+System.out.println(alphabets[2]);
+alphabets[4]='k';
+System.out.println(alphabets[4]);
+alphabets[5]='s';
+System.out.println(alphabets[5]);
+alphabets[6]='h';
+System.out.println(alphabets[6]);
+System.out.println("=========");
+for(int i=0;i<alphabets.length;i++)
+{
+System.out.println(alphabets[i]);
+}
+System.out.println("=========");
+for(int i=alphabets.length-1;i>0;i--)
+{
+System.out.println(alphabets[i]);
+}
+System.out.println("=========");
+for(int i=0;i<alphabets.length/2;i++)
+{
+System.out.println(alphabets[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<alphabets.length;i=i+3)
+{
+ System.out.println(alphabets[i]);
+}
+
+
+System.out.println("****************");
+float[] countpen={11,21,31,41,51,61,71,81,91,100};
+countpen[2]=12;
+System.out.println(countpen[2]);
+countpen[4]=31;
+System.out.println(countpen[4]);
+countpen[5]=43;
+System.out.println(countpen[5]);
+countpen[6]=56;
+System.out.println(countpen[6]);
+System.out.println("==============");
+
+for(int i=0;i<countpen.length;i++)
+{
+System.out.println(countpen[i]);
+}
+System.out.println("=========");
+for(int i=countpen.length-1;i>0;i--)
+{
+System.out.println(countpen[i]);
+}
+System.out.println("=========");
+for(int i=0;i<countpen.length/2;i++)
+{
+System.out.println(countpen[i]);
+}
+System.out.println("=========");
+ for(int i=0;i<countpen.length;i=i+3)
+{
+ System.out.println(countpen[i]);
+}
+}
+}
